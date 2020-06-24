8cfd9c1c751fa1d0916733b0d524f73cf7e58cfa
Sylvain Dégué <sylvain.degue@gmail.com>
2020-06-23 23:35:02 -0400

    update test file

--- tests/test_extract_business.py
+++ tests/test_extract_business.py
@@ -2,7 +2,7 @@ import os
 
 from app.businesses.extract import extract_business_from_csv
 
-BUSINESSES_CSV = os.path.join(os.path.dirname(__file__), 'businesses_file_upload.csv')
+BUSINESSES_CSV = os.path.join(os.path.dirname(__file__), 'businesses_file_extract.csv')
 
 
 def test_extract_business_from_csv(business, business_upload_file):
