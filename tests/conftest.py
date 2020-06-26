5de2a18658eededa49819898ef8f3f6a058bd5da
Sylvain Dégué <sylvain.degue@gmail.com>
2020-06-26 08:54:31 -0400

    update wip...

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,35 +6,46 @@ from app.businesses.repositories import BusinessRepository
 from app.config import TestingConfig
 
 
-@pytest.yield_fixture()
-def test_client():
-    flask_app = create_app(config=TestingConfig)
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
-    # Flask provides a way to test your application by exposing the Werkzeug test Client
-    # and handling the context locals for you.
-    testing_client = flask_app.test_client()
-
-    # Establish an application context before running the tests.
-    ctx = flask_app.app_context()
+@pytest.fixture(scope='session')
+def app():
+    _app = create_app(config=TestingConfig)
+    ctx = _app.app_context()
     ctx.push()
-
-    yield testing_client  # this is where the testing happens!
-
+    yield _app
     ctx.pop()
-@pytest.yield_fixture()
-def init_database(test_client):
-    # Create the database and the database table
-    db.create_all()
 
-    yield db  # this is where the testing happens!
 
+@pytest.fixture(scope="session")
+def _db(app):
+    """
+    Returns session-wide initialised database.
+    """
     db.drop_all()
-
-    db.session.close()
+    db.create_all()
+    yield db
+# @pytest.yield_fixture()
+# def test_client():
+#     app = create_app(config=TestingConfig)
+#     with app.test_client() as client:
+#         yield client
+# @pytest.yield_fixture()
+# def init_database(test_client):
+#     # Create the database and the database table
+#     db.create_all()
+#
+#     yield db  # this is where the testing happens!
+#
+#     db.drop_all()
+#
+#     db.session.close()
 
 @pytest.fixture()
 def category1():
-    category1 = Category(name="restaurant",id=1)
+    category1 = Category(name="restaurant")
     db.session.add(category1)
     db.session.flush()
     return category1
--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -5,14 +5,36 @@ from app.businesses.models import Category
 from app.businesses.repositories import BaseRepository
 
 
-def test_repository(test_client,init_database):
+def test_repository_query(app, _db):
     repository = BaseRepository()
     with pytest.raises(Exception):
         assert repository.query
 
 
-def test_repository_get_strict(test_client,init_database):
+def test_repository_get_strict(app, _db):
     repository = BaseRepository()
     repository.model = Category
     with pytest.raises(NotFound):
-        assert repository.get(1,error_message="shit",strict=True)
+        assert repository.get(100, error_message="shit", strict=True)
+
+
+def test_repository_filter(app, _db, category1):
+    repository = BaseRepository()
+    repository.model = Category
+    categories = repository.filter(**{"name":category1.name}).all()
+    assert len(categories) == 1
+    assert categories[0].name == category1.name
+
+def test_repository_save(app, _db):
+    repository = BaseRepository()
+    repository.model = Category
+    category = Category(name="BarberShop",id=10)
+    created_category = repository.save(category)
+    assert created_category.id is not None
+    assert created_category.name == "BarberShop"
+
+def test_repository_update(app, _db,category1):
+    repository = BaseRepository()
+    repository.model = Category
+    updated_category = repository.update(**{"name":"Brand new name"})
+    assert updated_category.name == "Brand new name"
