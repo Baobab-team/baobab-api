f2940756c472f4e5416091d92f49f7102c1695b0
Sylvain Dégué <sylvain.degue@gmail.com>
2020-07-09 17:45:29 -0400

    Refactoring

--- tests/test_extract_business.py
+++ tests/test_extract_business.py
@@ -2,10 +2,9 @@ import os
 
 from app.businesses.uploads import extract_business_from_csv
 
-BUSINESSES_CSV = os.path.join(os.path.dirname(__file__), 'businesses_file_extract.csv')
 
 
 def test_extract_business_from_csv(business, business_upload_file):
-    businesses = extract_business_from_csv(BUSINESSES_CSV)
+    businesses = extract_business_from_csv(business_upload_file)
 
     assert businesses[0] == business
